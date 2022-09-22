3de6752a9594e63ebb35fb0c63649b393542e790
PATIL Sapna <sapna.patil@soprasteria.com>
2022-09-22 11:11:29 +0530

    first commit

---
+++ src/com/sopra/bangloregit/Main.java
@@ -0,0 +1,17 @@
+package com.sopra.bangloregit;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Testcases test=new Testcases();
+		test.test1();
+		test.test2();
+		test.test3();
+	}
+
+	
+
+}
---
+++ src/com/sopra/bangloregit/Testcases.java
@@ -0,0 +1,18 @@
+package com.sopra.bangloregit;
+
+public class Testcases {
+	public void test1()
+	{
+		System.out.println("Test case 1");
+	}
+	public void test2()
+	{
+		System.out.println("Test case 2");
+	}
+	public void test3()
+	{
+		System.out.println("Test case 3");
+	}
+
+
+}
